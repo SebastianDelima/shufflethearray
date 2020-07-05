9699aa284402b9f173f0ddeafd2bd885d56eac20
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-07-05 13:25:28 -0400

    third solution

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ var shuffle = function(nums, n) {
     // }
     // return arr
 
-    let result =[];
-    for(let i=0;i < nums.length / 2; i++){
-        result.push(nums[i]);
-		result.push(nums[i + n]);
+    // let result =[];
+    // for(let i=0;i < nums.length / 2; i++){
+    //     result.push(nums[i]);
+	// 	result.push(nums[i + n]);
+    // }
+    // return result;
+
+    let answer = [];
+    for (let i = 0; i < nums.length / 2; i++) {
+        let j = i + n;
+        answer.push(nums[i]);
+        answer.push(nums[j]);
     }
-    return result;
+    return answer
 };
