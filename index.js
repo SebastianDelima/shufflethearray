68b0243779fa693acf0dce0752d527ed3bdef662
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-07-05 13:11:30 -0400

    solution

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 var shuffle = function(nums, n) {
     
-    2n{
-        #get this: [x1, x2, xn y1, y2, yn]
-            [x2 goes to nums[3]]
-            then{
-                nums[3] goes to x2
-            }
-    
-        #to this: [x1,y1,x2,y2,xn,yn]
+    let arr =[]
+    for(let i=0;i < n; i++){
+        arr.push(nums[i],nums[i + n])
     }
-    
-    4n{
-         #get this: [x1, x2, xn y1, y2, yn, 4, 4, 5, 5]
-         
-         #Each 
-    
-        #to this: [x1, y1, x2, y2, x3, y4, x4, y4, x5, y5]
-    }
-    
+    return arr
 };
