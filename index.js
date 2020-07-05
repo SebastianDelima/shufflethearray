add35205a1e2eabf42cfb352e33ca74f54652c71
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-07-05 12:47:53 -0400

    thinking

---
+++ index.js
@@ -0,0 +1,21 @@
+var shuffle = function(nums, n) {
+    
+    2n{
+        #get this: [x1, x2, xn y1, y2, yn]
+            [x2 goes to nums[3]]
+            then{
+                nums[3] goes to x2
+            }
+    
+        #to this: [x1,y1,x2,y2,xn,yn]
+    }
+    
+    4n{
+         #get this: [x1, x2, xn y1, y2, yn, 4, 4, 5, 5]
+         
+         #Each 
+    
+        #to this: [x1, y1, x2, y2, x3, y4, x4, y4, x5, y5]
+    }
+    
+};
